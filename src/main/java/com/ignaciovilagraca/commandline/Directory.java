0df8596d592c181ad057bca81ac6d4f8fa0d7fa5
Ignacio Vila <ivila@dc.uba.ar>
2023-02-01 14:20:55 -0300

    add packages

--- src/main/java/com/ignaciovilagraca/commandline/Directory.java
+++ src/main/java/com/ignaciovilagraca/commandline/Directory.java
@@ -1,4 +1,4 @@
-package main;
+package com.ignaciovilagraca.commandline;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/test/java/com/ignaciovilagraca/commandline/CommandLineTest.java
+++ src/test/java/com/ignaciovilagraca/commandline/CommandLineTest.java
@@ -1,5 +1,6 @@
+package com.ignaciovilagraca.commandline;
+
 import java.util.List;
-import main.CommandLine;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
